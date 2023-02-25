77c2c018126de63fc8c7cca477c1a5a2275825e6
colburncodes <colburnsanders@gmail.com>
2023-02-25 14:58:11 -0600

    fix: wrong import

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const userController = require("../controllers/user");
+const userController = require("./user");
 
 router.use("/users", userController);
 
