70d5d926894b0aa8874047ef23e423271d73c263
colburncodes <colburnsanders@gmail.com>
2023-04-13 21:27:46 -0500

    refactor: :recycle: refactor error handling

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
+const Status = require("../utils/error");
 
 const handleAuthError = (res) =>
-  res.status(401).send({ message: "Authorization Error" });
+  res.status(Status.Forbidden).send({ message: "Authorization Error" });
 
 const extractBearerToken = (header) => header.replace("Bearer", "");
 
@@ -9,14 +11,14 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res.status(401).send({ error: "Unauthorized" });
+    return res.status(Status.Forbidden).send({ error: "Unauthorized" });
   }
 
   const token = extractBearerToken(authorization);
   let payload;
 
   try {
-    payload = jwt.verify(token, "some-secret-key");
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     return handleAuthError(res);
   }
